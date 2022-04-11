63acf0577aa29fb657e9083bb629da0dd5b50838
17751332867 <ss@t1314>
2022-04-11 17:58:47 +0800

    wyl

--- src/components/CreateData.vue
+++ src/components/CreateData.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <div id="input" style="width: 40%;height: 500px;position: absolute;left: 0px;top: 50px">
-      <div style="position: absolute;top: 0px;left: 0px;width: 40%">
+      <div style="position: absolute;top: 0px;left: 0px;width: 40%;">
         <div style="color: dodgerblue;font-size: 25px">节点号</div>
         <div style="color: #aaaaaa">*每行一个节点</div>
-        <div style="width: 100%;height: 400px;overflow-y: scroll">
+        <div style="width: 100%;height: 380px;overflow-y: scroll;border: solid 1px #aaaaaa">
           <el-input
             type="textarea"
             :rows="10000"
@@ -14,10 +14,10 @@
           </el-input>
         </div>
       </div>
-      <div style="position: absolute;top: 0px;left: 50%;width: 40%">
+      <div style="position: absolute;top: 0px;left: 50%;width: 40%;">
         <div style="color: dodgerblue;font-size: 25px">连接边</div>
         <div style="color: #aaaaaa">*每行两个节点</div>
-        <div style="width: 100%;height: 400px;overflow-y: scroll">
+        <div style="width: 100%;height: 380px;overflow-y: scroll;border: solid 1px #aaaaaa">
           <el-input
             type="textarea"
             :rows="10000"
@@ -27,8 +27,31 @@
           </el-input>
         </div>
       </div>
+      <div style="position: absolute;bottom: 0px;left: 0px">
+        <el-row>
+          <el-col :span="8" style="font-size: 25px">文件名：</el-col>
+          <el-col :span="16"><el-input label="文件名" v-model="name"></el-input></el-col>
+        </el-row>
+      </div>
     </div>
-    <div id="chart" style="width:40%;height:500px;position: absolute;left: 50%;top: 50px"></div>
+    <div id="chart" style="width:40%;height:500px;position: absolute;left: 50%;top: 50px;border: solid 1px #aaaaaa"></div>
+    <el-row style="width: 100%;height: 100px;position: absolute;top: 600px;left: 0px">
+        <el-col :span="6">
+          <div>read长度:<el-input-number v-model="num" :step="50"></el-input-number></div>
+          <div>read个数:<el-input-number v-model="size" :step="10000"></el-input-number></div>
+        </el-col>
+        <el-col :span="8">
+          <div>核心DNA长度下界:<el-input-number v-model="maxLengthLowerBound" :step="100"></el-input-number></div>
+          <div>核心DNA长度上界:<el-input-number v-model="maxLengthUpperBound" :step="100"></el-input-number></div>
+        </el-col>
+        <el-col :span="8">
+          <div>附属DNA长度下界:<el-input-number v-model="minLengthLowerBound" :step="100"></el-input-number></div>
+          <div>附属DNA长度上界:<el-input-number v-model="minLengthUpperBound" :step="100"></el-input-number></div>
+        </el-col>
+        <el-col :span="2">
+          <el-button type="primary" @click="submit" style="height: 80px">提交</el-button>
+        </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -41,22 +64,54 @@ export default {
   data () {
     return {
       nodestr: '',
-      edgestr: ''
+      edgestr: '',
+      data: [],
+      links: [],
+      num: 100,
+      size: 10000,
+      maxLengthLowerBound: 3000,
+      maxLengthUpperBound: 5000,
+      minLengthLowerBound: 300,
+      minLengthUpperBound: 500,
+      name: ''
     }
   },
   methods: {
     changeChart () {
       let nodestr = this.nodestr
-      let data = []
+      this.data = []
       nodestr.split('\n').forEach(res => {
-        data.push({name: res})
+        if (res.length === 0) return
+        this.data.push({name: res})
       })
       let edgestr = this.edgestr
-      let links = []
+      console.log(edgestr.length)
+      this.links = []
+      // if (edgestr.length !== 0 || edgestr.split('\n').length === 1) {
+      //   this.$notify({message: '一行两个空格隔开', type: 'error'})
+      //   return
+      // }
+      edgestr.split('\n').forEach(res => {
+        if (res.length === 0) return
+        let tt = res.split(' ')
+        console.log(tt)
+        let ss = []
+        tt.forEach(t => {
+          if (t !== '')ss.push(t)
+        })
+        console.log(ss)
+        let source = ss[0]
+        let target = ss[1]
+        this.links.push({
+          source: source,
+          target: target,
+          name: ''
+        })
+      })
+      this.draw(this.data, this.links)
     },
-    draw () {
+    draw (chartNode, chartEdge) {
       var myChart = echarts.init(document.getElementById('chart'))
-      var categories = []
       var option
       option = {
         // 图的标题
@@ -124,35 +179,8 @@ export default {
           },
 
           // 数据
-          data: [{
-            name: 'node01'
-          }, {
-            name: 'node02'
-          }, {
-            name: 'node03'
-          }, {
-            name: 'node04'
-          }, {
-            name: 'node05'
-          }],
-          links: [{
-            source: 'node01',
-            target: 'node02',
-            name: ''
-          }, {
-            source: 'node01',
-            target: 'node03',
-            name: ''
-          }, {
-            source: 'node01',
-            target: 'node04',
-            name: ''
-          }, {
-            source: 'node01',
-            target: 'node05',
-            name: ''
-          }],
-          categories: categories
+          data: chartNode,
+          links: chartEdge
         }]
       }
       myChart.setOption(option)
